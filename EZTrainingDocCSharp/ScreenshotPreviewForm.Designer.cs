20138949bffc5e3fb8955939962611deb22c9040
Fluinol <ims_@hotmail.com>
2025-06-05 20:24:42 +0200

    Movidos botones a la nueva ventana. Falta ajustar funciones

--- EZTrainingDocCSharp/ScreenshotPreviewForm.Designer.cs
+++ EZTrainingDocCSharp/ScreenshotPreviewForm.Designer.cs
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.actionButtonsPanel = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.selectionControlPanel = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.chkSelectAll = new System.Windows.Forms.CheckBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.flowPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.actionButtonsPanel.SuspendLayout();
+            this.selectionControlPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // actionButtonsPanel
+            // 
+            this.actionButtonsPanel.Controls.Add(this.btnSave);
+            this.actionButtonsPanel.Controls.Add(this.btnChange);
+            this.actionButtonsPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.actionButtonsPanel.Location = new System.Drawing.Point(0, 0);
+            this.actionButtonsPanel.Name = "actionButtonsPanel";
+            this.actionButtonsPanel.Size = new System.Drawing.Size(800, 40);
+            this.actionButtonsPanel.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(136, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(103, 34);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save to Word";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(3, 3);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(127, 34);
+            this.btnChange.TabIndex = 0;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // selectionControlPanel
+            // 
+            this.selectionControlPanel.Controls.Add(this.btnDelete);
+            this.selectionControlPanel.Controls.Add(this.chkSelectAll);
+            this.selectionControlPanel.Controls.Add(this.btnClear);
+            this.selectionControlPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.selectionControlPanel.Location = new System.Drawing.Point(0, 40);
+            this.selectionControlPanel.Name = "selectionControlPanel";
+            this.selectionControlPanel.Size = new System.Drawing.Size(800, 40);
+            this.selectionControlPanel.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(245, 9);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete selection";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // chkSelectAll
+            // 
+            this.chkSelectAll.AutoSize = true;
+            this.chkSelectAll.Location = new System.Drawing.Point(93, 9);
+            this.chkSelectAll.Name = "chkSelectAll";
+            this.chkSelectAll.Size = new System.Drawing.Size(146, 20);
+            this.chkSelectAll.TabIndex = 1;
+            this.chkSelectAll.Text = "Select / deselect all";
+            this.chkSelectAll.UseVisualStyleBackColor = true;
+            this.chkSelectAll.CheckedChanged += new System.EventHandler(this.ChkSelectAll_CheckedChanged);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 7);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear all";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // flowPanel
+            // 
+            this.flowPanel.AutoScroll = true;
+            this.flowPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowPanel.Location = new System.Drawing.Point(0, 80);
+            this.flowPanel.Name = "flowPanel";
+            this.flowPanel.Size = new System.Drawing.Size(800, 370);
+            this.flowPanel.TabIndex = 2;
+            // 
+            // ScreenshotPreviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowPanel);
+            this.Controls.Add(this.selectionControlPanel);
+            this.Controls.Add(this.actionButtonsPanel);
+            this.Name = "ScreenshotPreviewForm";
             this.Text = "ScreenshotPreviewForm";
+            this.actionButtonsPanel.ResumeLayout(false);
+            this.selectionControlPanel.ResumeLayout(false);
+            this.selectionControlPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel actionButtonsPanel;
+        private System.Windows.Forms.Panel selectionControlPanel;
+        private System.Windows.Forms.FlowLayoutPanel flowPanel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.CheckBox chkSelectAll;
+        private System.Windows.Forms.Button btnDelete;
+
     }
 }
--- EZTrainingDocCSharp/ScreenshotPreviewForm.cs
+++ EZTrainingDocCSharp/ScreenshotPreviewForm.cs
@@ -8,62 +8,24 @@ namespace EZTrainingDocCSharp
     public partial class ScreenshotPreviewForm : Form
     {
         private List<Bitmap> screenshots;
-        private List<CheckBox> checkBoxes = new List<CheckBox>();
-        private FlowLayoutPanel flowPanel;
-        private CheckBox chkSelectAll;
-        private Button btnDelete;
+        private List<CheckBox> checkBoxes = new List<CheckBox>();         
+      
 
         public ScreenshotPreviewForm(List<Bitmap> screenshots)
         {
-            InitializeComponent();
+            InitializeComponent(); // Make sure this is called if you have a designer part
             this.screenshots = screenshots;
             InitializeUI();
         }
+
+        
 
         private void InitializeUI()
         {
             this.Text = "Screenshot Previews";
             this.Width = 800;
             this.Height = 600;
-
-            // Top panel for controls
-            var topPanel = new Panel
-            {
-                Dock = DockStyle.Top,
-                Height = 50
-            };
-
-            chkSelectAll = new CheckBox
-            {
-                Text = "Select/Deselect All",
-                Left = 10,
-                Top = 15,
-                Width = 150
-            };
-            chkSelectAll.CheckedChanged += ChkSelectAll_CheckedChanged;
-            topPanel.Controls.Add(chkSelectAll);
-
-            btnDelete = new Button
-            {
-                Text = "Delete Selected",
-                Left = 180,
-                Top = 10,
-                Width = 120,
-                Height = 30
-            };
-            btnDelete.Click += BtnDelete_Click;
-            topPanel.Controls.Add(btnDelete);
-
-            this.Controls.Add(topPanel);
-
-            // Flow panel for thumbnails
-            flowPanel = new FlowLayoutPanel
-            {
-                Dock = DockStyle.Fill,
-                AutoScroll = true,
-                WrapContents = true
-            };
-            this.Controls.Add(flowPanel);
+            this.StartPosition = FormStartPosition.CenterScreen;           
 
             PopulateThumbnails();
         }
@@ -75,7 +37,7 @@ namespace EZTrainingDocCSharp
 
             for (int i = 0; i < screenshots.Count; i++)
             {
-                var panel = new Panel { Width = 150, Height = 150, Margin = new Padding(5) };
+                var panel = new Panel { Width = 150, Height = 150, Margin = new Padding(5), BorderStyle = BorderStyle.FixedSingle };
 
                 var picBox = new PictureBox
                 {
@@ -83,17 +45,18 @@ namespace EZTrainingDocCSharp
                     SizeMode = PictureBoxSizeMode.Zoom,
                     Width = 120,
                     Height = 90,
-                    Top = 5,
-                    Left = 10
+                    Top = 10, // Added some top margin
+                    Left = (panel.Width - 120) / 2 // Center horizontally
                 };
                 panel.Controls.Add(picBox);
 
                 var checkBox = new CheckBox
                 {
                     Text = $"Screenshot {i + 1}",
-                    Top = 100,
+                    Top = picBox.Bottom + 10, // Position below picture box
                     Left = 10,
-                    Width = 120
+                    Width = panel.Width - 20, // Adjust width
+                    TextAlign = ContentAlignment.MiddleLeft
                 };
                 panel.Controls.Add(checkBox);
                 checkBoxes.Add(checkBox);
@@ -111,34 +74,36 @@ namespace EZTrainingDocCSharp
 
         private void BtnDelete_Click(object sender, EventArgs e)
         {
-            // Collect indices to delete (in reverse order to avoid shifting)
             var indicesToDelete = new List<int>();
             for (int i = 0; i < checkBoxes.Count; i++)
             {
                 if (checkBoxes[i].Checked)
                     indicesToDelete.Add(i);
             }
+
             if (indicesToDelete.Count == 0)
             {
                 MessageBox.Show("No screenshots selected for deletion.", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
 
-            // Confirm deletion
-            var result = MessageBox.Show($"Delete {indicesToDelete.Count} selected screenshot(s)?", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            var result = MessageBox.Show($"Are you sure you want to delete {indicesToDelete.Count} selected screenshot(s)?", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             if (result != DialogResult.Yes)
                 return;
 
-            // Delete from end to start to avoid index issues
-            indicesToDelete.Reverse();
+            indicesToDelete.Sort((a, b) => b.CompareTo(a)); // Sort descending to avoid index issues
+
             foreach (int idx in indicesToDelete)
             {
-                screenshots[idx].Dispose();
-                screenshots.RemoveAt(idx);
+                if (idx < screenshots.Count) // Ensure index is valid
+                {
+                    screenshots[idx].Dispose(); // Dispose the bitmap
+                    screenshots.RemoveAt(idx);
+                }
             }
 
-            PopulateThumbnails();
-            chkSelectAll.Checked = false;
+            PopulateThumbnails(); // Refresh the display
+            chkSelectAll.Checked = false; // Uncheck "Select All"
         }
 
         public List<int> GetSelectedIndices()
@@ -150,6 +115,47 @@ namespace EZTrainingDocCSharp
                     selected.Add(i);
             }
             return selected;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show(
+            "Are you sure you want to clear all screenshots?",
+            "Confirm Clear",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Warning);
+
+            if (result != DialogResult.Yes)
+                return;
+
+            // Dispose all bitmaps
+            foreach (var bmp in screenshots)
+            {
+                bmp.Dispose();
+            }
+            screenshots.Clear();
+            PopulateThumbnails();
+            chkSelectAll.Checked = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select a folder";
+                dialog.ShowNewFolderButton = true;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    // You can use dialog.SelectedPath as needed
+                    MessageBox.Show($"Selected folder: {dialog.SelectedPath}", "Folder Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    // TODO: Handle the selected folder path as needed in your application
+                }
+            }
         }
     }
 }
