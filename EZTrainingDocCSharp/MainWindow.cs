190e1dbbb1503cb5785cfa4a7840f6d1326fa0b5
Fluinol <ims_@hotmail.com>
2025-08-07 20:27:50 +0200

    encapsulated mainwindow better

--- EZTrainingDocCSharp/MainWindow.cs
+++ EZTrainingDocCSharp/MainWindow.cs
@@ -30,7 +30,20 @@ using System.Windows.Forms;
 namespace EZTrainingDocCSharp
 {
     public partial class MainWindow : Form
+
     {
+        //Class variables
+        //private string selectedFolderPath = string.Empty;
+        private string selectedFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private List<ScreenshotInfo> capturedScreenshotsList = new List<ScreenshotInfo>();
+        private bool isRecording = false;
+        private static Thread mouseListenerThread;
+        public static int maxScreenshotCount = 10; // Maximum number of screenshots to capture
+
+
+
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -38,39 +51,13 @@ namespace EZTrainingDocCSharp
                 this.Controls["lblStatus"].Text = "Ready.";
         }
 
-        //private string selectedFolderPath = string.Empty;
-        private string selectedFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        private List<ScreenshotInfo> capturedScreenshotsList = new List<ScreenshotInfo>();
-        private bool isRecording = false;
-        private static Thread mouseListenerThread;
+
 
 
 
         private void btnStop_Click(object sender, EventArgs e)
         {
-
-            if (isRecording)
-            {
-                isRecording = !isRecording;
-                //btnStartPause.Text = "Resume recording";
-                var recordImage = global::EZTrainingDocCSharp.Properties.Resources.rec48px.ToBitmap();
-                btnStartPause.Image = new Bitmap(recordImage, new Size(45, 45));
-                btnStartPause.ImageAlign = ContentAlignment.MiddleCenter;
-                this.Invalidate(); // Force redraw to update border
-            }
-
-
-            // Stop MouseListener only if running
-            Mouse.MouseListener.Stop();
-            if (mouseListenerThread != null && mouseListenerThread.IsAlive)
-            {
-                mouseListenerThread.Join(500); // Optionally wait for thread to finish
-                mouseListenerThread = null;
-            }
-
-            var previewForm = new ScreenshotPreviewForm(capturedScreenshotsList);
-            previewForm.ShowDialog(this);
-
+            Stop();
         }
 
         private void btnStartPause_Click(object sender, EventArgs e)
@@ -79,38 +66,13 @@ namespace EZTrainingDocCSharp
 
             if (isRecording)
             {
-                //btnStartPause.Text = "Pause";
-                var pauseImage = global::EZTrainingDocCSharp.Properties.Resources.pause48px.ToBitmap();
-                btnStartPause.Image = new Bitmap(pauseImage, new Size(45, 45));
-                btnStartPause.ImageAlign = ContentAlignment.MiddleCenter;
+                Start();
 
-
-
-
-                // Start MouseListener only if not already running
-                if (mouseListenerThread == null || !mouseListenerThread.IsAlive)
-                {
-                    //mouseListenerThread = new Thread(() => Mouse.MouseListener.Start(capturedScreenshotsList));
-                    //mouseListenerThread.IsBackground = true;
-                    Mouse.MouseListener.Start(capturedScreenshotsList);
-
-
-                }
             }
             else
             {
-                //btnStartPause.Text = "Resume recording";                
-                var recordImage = global::EZTrainingDocCSharp.Properties.Resources.rec48px.ToBitmap();
-                btnStartPause.Image = new Bitmap(recordImage, new Size(45, 45));
-                btnStartPause.ImageAlign = ContentAlignment.MiddleCenter;
-
-                // Stop MouseListener only if running
-                Mouse.MouseListener.Stop();
-                if (mouseListenerThread != null && mouseListenerThread.IsAlive)
-                {
-                    mouseListenerThread.Join(500); // Optionally wait for thread to finish
-                    mouseListenerThread = null;
-                }
+                Pause();
+                
             }
             this.Invalidate(); // Force redraw to update border
         }
@@ -149,6 +111,68 @@ namespace EZTrainingDocCSharp
                 //"GitHub Sponsors: https://github.com/sponsors/YOUR_PROFILE\n\n" +
                 "Thank you for your support!";
             MessageBox.Show(message, "Support Development", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //STOP;START;PAUSE
+        private void Stop()
+        {
+            if (isRecording)
+            {
+                isRecording = !isRecording;
+                //btnStartPause.Text = "Resume recording";
+                var recordImage = global::EZTrainingDocCSharp.Properties.Resources.rec48px.ToBitmap();
+                btnStartPause.Image = new Bitmap(recordImage, new Size(45, 45));
+                btnStartPause.ImageAlign = ContentAlignment.MiddleCenter;
+                this.Invalidate(); // Force redraw to update border
+            }
+
+
+            // Stop MouseListener only if running
+            Mouse.MouseListener.Stop();
+            if (mouseListenerThread != null && mouseListenerThread.IsAlive)
+            {
+                mouseListenerThread.Join(500); // Optionally wait for thread to finish
+                mouseListenerThread = null;
+            }
+
+            var previewForm = new ScreenshotPreviewForm(capturedScreenshotsList);
+            previewForm.ShowDialog(this);
+        }
+        private void Start()
+        {
+            //btnStartPause.Text = "Pause";
+            var pauseImage = global::EZTrainingDocCSharp.Properties.Resources.pause48px.ToBitmap();
+            btnStartPause.Image = new Bitmap(pauseImage, new Size(45, 45));
+            btnStartPause.ImageAlign = ContentAlignment.MiddleCenter;
+
+
+
+
+            // Start MouseListener only if not already running
+            if (mouseListenerThread == null || !mouseListenerThread.IsAlive)
+            {
+                //mouseListenerThread = new Thread(() => Mouse.MouseListener.Start(capturedScreenshotsList));
+                //mouseListenerThread.IsBackground = true;
+                Mouse.MouseListener.Start(capturedScreenshotsList);
+
+
+            }
+        }
+
+        private void Pause()
+        {
+            //btnStartPause.Text = "Resume recording";                
+            var recordImage = global::EZTrainingDocCSharp.Properties.Resources.rec48px.ToBitmap();
+            btnStartPause.Image = new Bitmap(recordImage, new Size(45, 45));
+            btnStartPause.ImageAlign = ContentAlignment.MiddleCenter;
+
+            // Stop MouseListener only if running
+            Mouse.MouseListener.Stop();
+            if (mouseListenerThread != null && mouseListenerThread.IsAlive)
+            {
+                mouseListenerThread.Join(500); // Optionally wait for thread to finish
+                mouseListenerThread = null;
+            }
         }
     }
 }
